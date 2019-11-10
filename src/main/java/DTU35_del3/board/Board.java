74fe705b6be332b2dd30d721657e8fed79200877
MerithzDTU <s195480@student.dtu.dk>
2019-11-10 18:09:26 +0100

    Oprettet Street objekter og kommenteret "message" ud. + omdøbt Square til Street

--- src/main/java/DTU35_del3/board/Board.java
+++ src/main/java/DTU35_del3/board/Board.java
@@ -1,22 +1,27 @@
 package DTU35_del3.board;
 
 public class Board {
-    private Message message;
-    private Street[] Streets = new Street[11];
-    public Board(String language) {
-        message = new Message(language);
+//    private Message message;
+    private Street[] Streets = new Street[14];
+    public Board() {
+//        message = new Message(language);
 
-        Streets[0] = new Street("Start", message.towerMessage(), 250, 0);
-        Streets[1] = new Street("Crater", message.craterMessage(), -100, 1);
-        Streets[2] = new Street("Palace gates", message.palaceGatesMessage(), 100, 2);
-        Streets[3] = new Street("Cold Desert", message.coldDesertMessage(), -20, 3);
-        Streets[4] = new Street("Walled city", message.walledCityMessage(), 180, 4);
-        Streets[5] = new Street("Monastery", message.monasteryMessage(), 0, 5);
-        Streets[6] = new Street("Black cave", message.blackCaveMessage(), -70, 6);
-        Streets[7] = new Street("Huts in the mountain", message.hutsInTheMountainMessage(), 60, 7);
-        Streets[8] = new Street("The Werewall (werewolf-wall)", message.theWerewallMessage(), -80, 8);
-        Streets[9] = new Street("The pit", message.thePitMessage(), -50, 9);
-        Streets[10] = new Street("Goldmine", message.goldmineMessage(), 650, 10);
+        Streets[0] = new Street("Burgerbaren", -1, 0);
+        Streets[1] = new Street("Pizzeriaet", -1, 1);
+        Streets[2] = new Street("Slikbutikken", -1, 2);
+        Streets[3] = new Street("Iskiosken",-1, 3);
+        Streets[4] = new Street("Museet",-2, 4);
+        Streets[5] = new Street("Biblioteket",-2, 5);
+        Streets[6] = new Street("Skaterparken",-2, 6);
+        Streets[7] = new Street("Swimmingpoolen",-2, 7);
+        Streets[8] = new Street("Spillehallen",-3, 8);
+        Streets[9] = new Street("Biografen",-3, 9);
+        Streets[10] = new Street("Legetøjsbutikken",-3, 10);
+        Streets[11] = new Street("Dyrehandlen",-3, 11);
+        Streets[12] = new Street("Bowlinghallen",-4, 12);
+        Streets[13] = new Street("Zoo",-4, 13);
+        Streets[14] = new Street("Vandlandet", -5, 14);
+        Streets[15] = new Street("Strandpromenaden",-5, 15);
     }
 
     public String getStreetName(int StreetNumber) {
@@ -34,31 +39,31 @@ public class Board {
         return StreetNumber;
     }
 
-    public String getStreetMessage(int StreetNumber) {
-        String message = Streets[StreetNumber].getMessage();
-        return message;
-    }
+//    public String getStreetMessage(int StreetNumber) {
+//        String message = Streets[StreetNumber].getMessage();
+//        return message;
+//    }
 
-    public String getStreetMessage(String StreetName) {
-        String message = "";
-        for (Street a: Streets) {
-            if (StreetName == a.getName()) {
-                message = a.getMessage();
-            }
-        }
-        return message;
-    }
+//    public String getStreetMessage(String StreetName) {
+//        String message = "";
+//        for (Street a: Streets) {
+//            if (StreetName == a.getName()) {
+//                message = a.getMessage();
+//            }
+//        }
+//        return message;
+//    }
 
-    public int getStreetCashPrize(int StreetNumber) {
-        int cash = Streets[StreetNumber].getCashPrize();
+    public int getStreetCashPrice(int StreetNumber) {
+        int cash = Streets[StreetNumber].getCashPrice();
         return cash;
     }
 
-    public int getStreetCashPrize(String StreetName) {
+    public int getStreetCashPrice(String StreetName) {
         int cash = 404;
         for (Street a: Streets) {
             if (StreetName == a.getName()) {
-                cash = a.getCashPrize();
+                cash = a.getCashPrice();
             }
         }
         return cash;

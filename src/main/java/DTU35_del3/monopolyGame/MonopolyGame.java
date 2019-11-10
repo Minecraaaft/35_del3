0b5cd1b203f7ba57ebda0dee42cbb5901ec0c693
Johan Kryger <johankrygerr@gmail.com>
2019-11-10 19:55:01 +0100

    Random array

--- src/main/java/DTU35_del3/monopolyGame/MonopolyGame.java
+++ src/main/java/DTU35_del3/monopolyGame/MonopolyGame.java
@@ -1,4 +1,34 @@
 package DTU35_del3.monopolyGame;
 
+import java.util.Random;
+
 public class MonopolyGame {
+
+
+
+    //Creates a 20 long array with random numbers from 1 to 20. called "chanceArr"
+    int[] chanceArr = new int[20];
+    Random ran = new Random();
+    boolean inArray = false;
+    int counter = 0;
+
+        while (chanceArr[19] == 0) {
+        int ranNumber = ran.nextInt(21);
+
+        for (int i = 0; i < chanceArr.length; i++) {
+            if (chanceArr[i] == ranNumber) {
+                inArray = true;
+            }
+        }
+        if (!inArray) {
+            chanceArr[counter] = ranNumber;
+            counter++;
+        }
+        inArray = false;
+    }
+}
+
+
+
+
 }

67a1457f3143999b3f9824d68ac573024de0903a
Minecraaaft <maxmagi@hotmail.com>
2019-11-10 19:33:46 +0100

    added players to MonopolyGame

--- src/main/java/DTU35_del3/monopolyGame/MonopolyGame.java
+++ src/main/java/DTU35_del3/monopolyGame/MonopolyGame.java
@@ -1,11 +1,11 @@
 package DTU35_del3.monopolyGame;
 
 import DTU35_del3.board.Board;
+import DTU35_del3.player.Player;
 
 public class MonopolyGame {
-    public static void main(String[] args) {
-        Board board = new Board();
-        System.out.println(board);
-
-    }
+    private Player player1 = new Player();
+    private Player player2 = new Player();
+    private Player player3 = new Player();
+    private Player player4 = new Player();
 }

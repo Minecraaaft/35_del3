a64de6cca6a20245265ef70d5337934967d1cba0
Minecraaaft <maxmagi@hotmail.com>
2019-11-10 20:03:36 +0100

    fixet johans fejl

--- src/main/java/DTU35_del3/monopolyGame/MonopolyGame.java
+++ src/main/java/DTU35_del3/monopolyGame/MonopolyGame.java
@@ -8,34 +8,32 @@ import java.util.Random;
 public class MonopolyGame {
 
 
-
-    //Creates a 20 long array with random numbers from 1 to 20. called "chanceArr"
-    int[] chanceArr = new int[20];
-    Random ran = new Random();
-    boolean inArray = false;
-    int counter = 0;
-
-        while (chanceArr[19] == 0) {
-        int ranNumber = ran.nextInt(21);
-
-        for (int i = 0; i < chanceArr.length; i++) {
-            if (chanceArr[i] == ranNumber) {
-                inArray = true;
-            }
-        }
-        if (!inArray) {
-            chanceArr[counter] = ranNumber;
-            counter++;
-        }
-        inArray = false;
-    }
-
-
-
-
-
     private Player player1 = new Player();
     private Player player2 = new Player();
     private Player player3 = new Player();
     private Player player4 = new Player();
+
+
+    public void johansmetode() {
+        //Creates a 20 long array with random numbers from 1 to 20. called "chanceArr"
+        int[] chanceArr = new int[20];
+        Random ran = new Random();
+        boolean inArray = false;
+        int counter = 0;
+
+        while (chanceArr[19] == 0) {
+            int ranNumber = ran.nextInt(21);
+
+            for (int i = 0; i < chanceArr.length; i++) {
+                if (chanceArr[i] == ranNumber) {
+                    inArray = true;
+                }
+            }
+            if (!inArray) {
+                chanceArr[counter] = ranNumber;
+                counter++;
+            }
+            inArray = false;
+        }
+    };
 }

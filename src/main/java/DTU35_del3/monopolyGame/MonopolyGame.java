b3f066a4faa8e45eea2708163dce20674f7d3c74
Bendorff <andreasbendorff@hotmail.com>
2019-11-15 11:12:11 +0100

    Gui Start

--- src/main/java/DTU35_del3/monopolyGame/MonopolyGame.java
+++ src/main/java/DTU35_del3/monopolyGame/MonopolyGame.java
@@ -2,7 +2,14 @@ package DTU35_del3.monopolyGame;
 
 import DTU35_del3.board.Board;
 import DTU35_del3.player.Player;
+import gui_fields.GUI_Brewery;
+import gui_fields.GUI_Field;
+import gui_fields.GUI_Start;
+import gui_fields.GUI_Street;
+import gui_main.GUI;
 
+import javax.swing.*;
+import java.awt.*;
 import java.util.Random;
 
 public class MonopolyGame {
@@ -13,6 +20,16 @@ public class MonopolyGame {
     private Player player3 = new Player();
     private Player player4 = new Player();
 
+    private GUI_Field[] fields = {
+            new GUI_Start("START", "", "",  Color.RED, Color.WHITE),
+            new GUI_Brewery("35_del3/src/main/java/DTU35_del3/Pictures/Burger_final.jpg", "Burgerbaren", "1M","","", Color.RED, Color.BLACK)
+
+
+    };
+
+    private GUI gui = new GUI(fields);
+
+    public void Start() {}
 
     public int[] johansmetode() {
         //Creates a 20 long array with random numbers from 1 to 20. called "chanceArr"
--- src/main/java/main/Main.java
+++ src/main/java/main/Main.java
@@ -1,17 +1,12 @@
 package main;
 
+import DTU35_del3.monopolyGame.MonopolyGame;
 import gui_fields.GUI_Field;
 import gui_fields.GUI_Player;
 import gui_main.GUI;
 
 public class Main {
     public static void main(String[] args) {
-        GUI gui = new GUI();
-        GUI_Player p1 = new GUI_Player("max");
-        GUI_Player p2 = new GUI_Player("mo");
-        gui.addPlayer(p1);
-        gui.addPlayer(p2);
-        GUI_Field gf[] = gui.getFields();
-        System.out.println(gf[0].getTitle());
+        new MonopolyGame().Start();
     }
 }

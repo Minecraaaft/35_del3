8425804a51beef6f722b94826a9be07207a55f36
max <maxmagi@hotmail.com>
2019-11-24 18:29:31 +0100

    johan fix2

--- src/test/java/test/DiceCupTest.java
+++ src/test/java/test/DiceCupTest.java
@@ -14,63 +14,45 @@ public class DiceCupTest {
     @Test
     public void rollDice() {
         for (int i = 0; i < 10000; i++) {
-            assertEquals(7, myTestDicecup.rollDice(), 5);
+            assertEquals(3.5, myTestDicecup.rollDice(), 2.5);
         }
     }
 
     @Test
     public void rollDiceOutcome() {
-        int[] arr = new int[11];
+        int[] arr = new int[6];
 
         for (int i = 0; i < 36000; i++) {
             int value = myTestDicecup.rollDice();
             switch (value) {
-                case 2:
+                case 1:
                     arr[0]++;
                     break;
-                case 3:
+                case 2:
                     arr[1]++;
                     break;
-                case 4:
+                case 3:
                     arr[2]++;
                     break;
-                case 5:
+                case 4:
                     arr[3]++;
                     break;
-                case 6:
+                case 5:
                     arr[4]++;
                     break;
-                case 7:
+                case 6:
                     arr[5]++;
                     break;
-                case 8:
-                    arr[6]++;
-                    break;
-                case 9:
-                    arr[7]++;
-                    break;
-                case 10:
-                    arr[8]++;
-                    break;
-                case 11:
-                    arr[9]++;
-                    break;
-                case 12:
-                    arr[10]++;
-                    break;
+
             }
         }
-        assertEquals(1/36.*36000, arr[0], 1/150.*36000);
-        assertEquals(2/36.*36000, arr[1], 1/150.*36000);
-        assertEquals(3/36.*36000, arr[2], 1/150.*36000);
-        assertEquals(4/36.*36000, arr[3], 1/150.*36000);
-        assertEquals(5/36.*36000, arr[4], 1/150.*36000);
-        assertEquals(6/36.*36000, arr[5], 1/150.*36000);
-        assertEquals(5/36.*36000, arr[6], 1/150.*36000);
-        assertEquals(4/36.*36000, arr[7], 1/150.*36000);
-        assertEquals(3/36.*36000, arr[8], 1/150.*36000);
-        assertEquals(2/36.*36000, arr[9], 1/150.*36000);
-        assertEquals(1/36.*36000, arr[10], 1/150.*36000);
+        assertEquals(1/36.*36000, arr[0], 1/6.*36000);
+        assertEquals(2/36.*36000, arr[1], 1/6.*36000);
+        assertEquals(3/36.*36000, arr[2], 1/6.*36000);
+        assertEquals(4/36.*36000, arr[3], 1/6.*36000);
+        assertEquals(5/36.*36000, arr[4], 1/6.*36000);
+        assertEquals(6/36.*36000, arr[5], 1/6.*36000);
+
     }
 
 }

33e99c41ee9972a9c9978fdf553529b85e446b72
max <maxmagi@hotmail.com>
2019-11-29 12:42:01 +0100

    removed getMessage and inserted names into board names

---
+++ src/test/java/35_del3/AppTest.java
@@ -0,0 +1,20 @@
+//package 35_del3;
+//
+//import static org.junit.Assert.assertTrue;
+//
+//import org.junit.Test;
+//
+///**
+// * Unit test for simple App.
+// */
+//public class AppTest
+//{
+//    /**
+//     * Rigorous Test :-)
+//     */
+//    @Test
+//    public void shouldAnswerWithTrue()
+//    {
+//        assertTrue( true );
+//    }
+//}
